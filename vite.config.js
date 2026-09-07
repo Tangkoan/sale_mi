1a30f0361a48f6106e83ee8f6c3e9a97b9dd6b81
kuytangkoan <kuytangkoan@gmail.com>
2026-09-07 11:41:43 +0700

    Update file vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,3 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
-
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -21,12 +8,25 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    // បន្ថែមផ្នែក server នេះចូល
-    server: {
-        host: '0.0.0.0',
-        cors: true,
-        hmr: {
-            host: '192.168.1.2', // ដាក់ IP កុំព្យូទ័ររបស់អ្នកនៅទីនេះ
-        },
-    },
 });
+
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+//     // បន្ថែមផ្នែក server នេះចូល
+//     server: {
+//         host: '0.0.0.0',
+//         cors: true,
+//         hmr: {
+//             host: '192.168.1.2', // ដាក់ IP កុំព្យូទ័ររបស់អ្នកនៅទីនេះ
+//         },
+//     },
+// });
